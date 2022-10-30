5d5ab7fa99bcd4a592c375ba549816ede1b0a698
IonutParau <ionut.alex.parau@gmail.com>
2022-10-30 17:33:08 +0100

    Attempt to fix windows bug

--- lib/ui/play_button.dart
+++ lib/ui/play_button.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:photon_launcher/data/version_manager.dart';
 import 'package:window_manager/window_manager.dart';
@@ -13,9 +15,15 @@ class PlayVersionButton extends StatelessWidget {
     return MaterialButton(
       onPressed: () async {
         if (!VersionManager.instance.hasVersion(v)) return;
-        await windowManager.hide();
-        await VersionManager.instance.runVersion(v);
-        await windowManager.show();
+        if (Platform.isWindows) {
+          await windowManager.minimize();
+          await VersionManager.instance.runVersion(v);
+          await windowManager.maximize();
+        } else {
+          await windowManager.hide();
+          await VersionManager.instance.runVersion(v);
+          await windowManager.show();
+        }
       },
       color: Colors.blue,
       child: Text("Play", style: TextStyle(fontSize: winSize.width / 40, color: Colors.white)),
